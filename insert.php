2ba60583a46785254806c96fb5ca2489f8e37408
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-05-09 13:52:54 +0200

    ajustement_message_erreur

--- insert.php
+++ insert.php
@@ -4,7 +4,7 @@
 
 if ((!empty($_POST["nom"] && $_POST["prenom"] && $_POST["code_postal"] && $_POST["celibataire"] && $_POST["salaire"] && $niveau = $_POST["niveau"])) && (preg_match("#^[0-9]{5}$#",$_POST['code_postal']))) {
 
-            $plane = htmlspecialchars($_POST["nom"]);
+            $nom = htmlspecialchars($_POST["nom"]);
             $prenon = htmlspecialchars($_POST["prenom"]);
             $code_postal = intval($_POST["code_postal"]);
             $celibataire = $_POST["celibataire"];
@@ -17,53 +17,79 @@ if ((!empty($_POST["nom"] && $_POST["prenom"] && $_POST["code_postal"] && $_POST
 
     
 }else{
-    header("Location: index.php");
-    // echo "vous devez mettre nom";
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    <?php require_once "partials/header.php" ?>
+    
+    <div class="text-center">
+
+        <h1>problème</h1>
+            <p>veuillez verifier les champs</p>
+    </div>
+    </body>
+    </html>
+
+    <?php
 }
 
+// teste car j'ai essayer de rejouter des controle mais j'ai eu des complication j'ai preferer y revenir plustard si j'ai le temps
 
-var_dump(is_string(intval($_POST["nom"])));
-var_dump(is_string($prenon));
-var_dump(gettype(intval($code_postal)));
-var_dump(is_string($code_postal));
-var_dump(gettype(intval($salaire)));
-var_dump(gettype($salaire));
-echo $salaire;
+// var_dump(is_string(intval($_POST["nom"])));
+// var_dump(is_string($prenon));
+// var_dump(gettype(intval($code_postal)));
+// var_dump(is_string($code_postal));
+// var_dump(gettype(intval($salaire)));
+// var_dump(is_string($salaire));
+// echo $salaire;
+
+// -------------------------------------insertion BDD---------------------
+
 // je stock la date et l'heure actuelle dans une variable
 $date_creation = date('d-m-y h:i:s');
 
 // ouvertur d'une connexion a la BDD
 
-// $objetPDO = new PDO('mysql:host=localhost;dbname=gta_rp','root','');
+$objetPDO = new PDO('mysql:host=localhost;dbname=gta_rp','root','');
 
 // Préparer la requete d'insertion 
 
-// $pdoStat = $objetPDO->prepare('INSERT INTO utilisateur VALUES (NULL, :nom, :prenom, :code_postal, :celibataire, :salaire, :photo, :niveau, :date_creation)');
+$pdoStat = $objetPDO->prepare('INSERT INTO utilisateur VALUES (NULL, :nom, :prenom, :code_postal, :celibataire, :salaire, :photo, :niveau, :date_creation)');
 
 // je lie chaque marqueur à une valeur
 
-// $pdoStat->bindValue(':nom', $_POST["nom"], PDO::PARAM_STR);
-// $pdoStat->bindValue(':prenom', $_POST["prenom"], PDO::PARAM_STR);
-// $pdoStat->bindValue(':code_postal', $_POST["code_postal"], PDO::PARAM_INT);
-// $pdoStat->bindValue(':celibataire', $_POST["celibataire"], PDO::PARAM_BOOL);
-// $pdoStat->bindValue(':salaire', $_POST["salaire"], PDO::PARAM_INT);
-// $pdoStat->bindValue(':photo', $_POST["photo"], PDO::PARAM_STR);
-// $pdoStat->bindValue(':niveau', $_POST["niveau"], PDO::PARAM_INT);
-// $pdoStat->bindValue(':date_creation', $date_creation);
+if (isset($nom)) {
+    # code...
+    $pdoStat->bindValue(':nom', $nom, PDO::PARAM_STR);
+    $pdoStat->bindValue(':prenom', $prenon, PDO::PARAM_STR);
+    $pdoStat->bindValue(':code_postal', $code_postal, PDO::PARAM_INT);
+    $pdoStat->bindValue(':celibataire', $celibataire, PDO::PARAM_BOOL);
+    $pdoStat->bindValue(':salaire', $salaire, PDO::PARAM_INT);
+    $pdoStat->bindValue(':photo', $photo, PDO::PARAM_STR);
+    $pdoStat->bindValue(':niveau', $niveau, PDO::PARAM_INT);
+    $pdoStat->bindValue(':date_creation', $date_creation);
+}
+
 
 // execution de la requete préparée
 
-// $insertIsOk = $pdoStat->execute();
+$insertIsOk = $pdoStat->execute();
 
-// if($insertIsOk){
-//     $message = 'le contact a ete ajouter a la BDD';
-// }else{
-//     $message = 'echec de l\insertion';
-// }
-
+if($insertIsOk){
+    $message = 'le contact a ete ajouter a la BDD';
+}else{
+    $message = "echec de l'insertion";
+}
 ?>
 
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -72,8 +98,15 @@ $date_creation = date('d-m-y h:i:s');
     <title>Document</title>
 </head>
 <body>
+<?php require_once "partials/header.php" ?>
 
-<h1>Insertion des données</h1>
-    <p><?php echo $message; ?></p>
+<div class="container text-center">
+
+    
+        <p><?=$message?></p>
+</div>
 </body>
-</html> -->
+</html>
+
+
+
